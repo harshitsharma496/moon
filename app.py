2536d233d80da9e2567623f276b1a880de553d0b
harshitsharma496 <98702386+harshitsharma496@users.noreply.github.com>
2026-01-12 17:22:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain_core.runnables import RunnablePassthrough
 
 # 1. Page Setup
 st.set_page_config(page_title="Silina Mishra", layout="wide")
-st.title("🌙 Welcome to MoonGpt💗⃝🌕", anchor=False)
+st.title("Welcome to MoonGpt 💗⃝🌕", anchor=False)
 
 # 2. Secure API Key Handling
 # We look for the key in Streamlit Secrets (Cloud)
